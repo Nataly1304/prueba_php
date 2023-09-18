cc2011eedfcb8debbd5b8b95f3dc82b8e5038d3f
Nataly1304 <natalima800@gmail.com>
2023-09-18 07:32:05 -0500

    ejerphp

---
+++ conexion.php
@@ -0,0 +1,16 @@
+<?php
+
+$conexion = new mysqli('localhost','root','','prueba_datos');
+if ($conexion->connect_errno){
+    die ('lo siento hubo un problema en la coneccion');
+
+    } else{
+        $sql = "INSERT INTO usuarios(id,nombre,edad) VALUES(null,'sonia',30)";
+        $resultado = $conexion->query($sql);
+        if($conexion->affected_rows >=1){
+            echo 'filas agredas: ' . $conexion->affected_rows;
+
+    }
+}
+
+?>
---
+++ leer_datos.php
@@ -0,0 +1,22 @@
+<?php
+$conexion = new mysqli('localhost','root','','prueba_datos');
+if ($conexion->connect_errno){
+    die ('lo siento hubo un problema en la coneccion');
+}else{
+    $sql ="SELECT * FROM USUARIOS";
+    $resultado = $conexion->query($sql);
+
+    if ($resultado->num_rows){
+        while($fila=$resultado->fetch_assoc()){
+
+            echo $fila ['id'].' '. $fila ['nombre']. '<br/>';
+
+        }
+    }
+        else{
+            echo 'no hay datos';
+        }
+    }
+
+
+?>
